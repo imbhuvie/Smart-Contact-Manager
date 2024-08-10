fe74931778131d07da1b9188bfdf94268d4e7025
Bhupendra Verma <bhupendraverma7388@gmail.com>
2024-08-10 23:37:16 +0530

    I have completed my light, dark theme button to work properly even if we change our pages.

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,41 @@
+const htmlTag = document.querySelector("html");
+const theme_button = document.querySelector(".theme_button");
+
+let currentTheme = getThemeType();
+console.log(currentTheme);
+// Step 1: set theme to localStorage
+function setThemeType(theme) {
+  localStorage.setItem("theme", theme);
+}
+// Step 2: get the theme from localStorage
+function getThemeType() {
+  let theme = localStorage.getItem("theme");
+  return theme ? theme : "light";
+}
+
+// step 3: change theme when document(content of HTML) fully Loaded .
+document.addEventListener("DOMContentLoaded", () => {
+  // we change theme, Initially called when page loaded.
+  changeTheme();
+});
+
+function changeTheme() {
+  // set initial theme for webpage
+  htmlTag.classList.add(currentTheme);
+
+  // Action performed when click on theme button
+  theme_button.addEventListener("click", () => {
+    const oldTheme = currentTheme;
+    if (currentTheme == "light") {
+      currentTheme = "dark";
+      theme_button.style.color = "white";
+    } else {
+      currentTheme = "light";
+      theme_button.style.color = "black";
+    }
+    setThemeType(currentTheme);
+
+    htmlTag.classList.remove(oldTheme);
+    htmlTag.classList.add(currentTheme);
+  });
+}
